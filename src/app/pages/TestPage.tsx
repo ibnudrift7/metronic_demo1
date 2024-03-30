3556ae8bded44c20433fc2d7be8d08d12077dcd6
ibnu fajar <ibnudrift@gmail.com>
2024-03-30 15:40:21 +0700

    first commit

---
+++ src/app/pages/TestPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {Content} from "../../_metronic/layout/components";
+                
+const TestPage = () => {
+    return (
+        <Content>
+            Hello, lasflksdjf
+        </Content>
+    )
+}
+
+export default TestPage;
